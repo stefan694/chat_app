9b3ad7c7e786ce3a6b966deb04a2974d0cf41b38
Stefan Atkinson <stefan.atkinson69@gmail.com>
2016-10-02 18:35:17 +0200

    added ActionCable on messages

---
+++ app/channels/chatrooms_channel.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class ChatroomsChannel < ApplicationCable::Channel
+  def subscribed
+    current_user.chatrooms.each do | chatroom |
+      stream_from "chatrooms:#{chatroom.id}"
+    end
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+  end
+end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,8 @@ class MessagesController < ApplicationController
   def create
     message = @chatroom.messages.new(message_params)
     message.user = current_user
+    message.save
+    MessageRelayJob.perform_later(message)
   end
 
   private
